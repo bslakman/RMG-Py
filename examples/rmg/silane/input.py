62e8ca43a8ced4fff6c2428c06d4e918a04f3969
Belinda Slakman <slakman.b@husky.neu.edu>
2014-10-10 14:43:20 -0700

    Silane input file
    Test silylene_elimination family

--- examples/rmg/silane/input.py
+++ examples/rmg/silane/input.py
@@ -4,7 +4,7 @@ database(
     reactionLibraries = [],
     seedMechanisms = [],
     kineticsDepositories = ['training'],
-    kineticsFamilies = ['H_Abstraction', 'Silylene_Insertion'],
+    kineticsFamilies = ['Silylene_Elimination'],
     kineticsEstimator = 'rate rules',
 )
 
@@ -12,7 +12,13 @@ database(
 species(
     label='SiH4',
     reactive=True,
-    structure=SMILES("[SiH4]")
+    structure=adjacencyList("""
+	1 Si u0 p0 c0 {2,S} {3,S} {4,S} {5,S}
+	2 H u0 p0 c0 {1,S}
+	3 H u0 p0 c0 {1,S}
+	4 H u0 p0 c0 {1,S}
+	5 H u0 p0 c0 {1,S}
+	""")
 )
 
 species(
@@ -21,13 +27,27 @@ species(
     structure=SMILES("[H][H]")
 )
 
+species(
+    label='SiH3SiH',
+    reactive=True,
+    structure=adjacencyList("""
+	1 Si u0 p1 c0 {2,S} {3,S}
+	2 H u0 p0 c0 {1,S}
+	3 Si u0 p0 c0 {1,S} {4,S} {5,S} {6,S}
+	4 H u0 p0 c0 {3,S}
+	5 H u0 p0 c0 {3,S}
+	6 H u0 p0 c0 {3,S}
+	""")
+)
+
 # Reaction systems
 simpleReactor(
     temperature=(800,'K'),
     pressure=(1.0,'bar'),
     initialMoleFractions={
-        "SiH4": 0.9,
-	"H2": 0.1
+        "SiH4": 0.8,
+	"H2": 0.1,
+	"SiH3SiH": 0.1
     },
     terminationConversion={
         'SiH4': 0.9,
@@ -42,7 +62,7 @@ simulator(
 
 model(
     toleranceKeepInEdge=0.0,
-    toleranceMoveToCore=0.1,
+    toleranceMoveToCore=0.01,
     toleranceInterruptSimulation=0.1,
     maximumEdgeSpecies=100000
 )
