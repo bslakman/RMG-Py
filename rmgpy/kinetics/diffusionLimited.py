62b3846dc09a9967b70f3128d843dd99d1aa9b3f
Richard West <r.west@neu.edu>
2016-04-08 16:31:46 -0400

    Fix integer arithmetic **(1/3) bug in getDiffusionLimit
    In Python 2, (1/3) evaluates to 0 because they're both integers,
    so the radius was always 1cm.
    Also added units comment to docstring

--- rmgpy/kinetics/diffusionLimited.py
+++ rmgpy/kinetics/diffusionLimited.py
@@ -117,6 +117,7 @@ class DiffusionLimited():
         
         This is the upper limit on the rate, in the specified direction.
         (ie. forward direction if forward=True [default] or reverse if forward=False)
+        Returns the rate coefficient k_diff in m3/mol/s.
         """
         if forward:
             reacting = reaction.reactants
@@ -133,12 +134,12 @@ class DiffusionLimited():
                 logging.warning("Species didn't have soluteData.")
                 soluteData = self.database.getSoluteData(spec)
             # calculate radius with the McGowan volume and assuming sphere
-            radius = ((75*soluteData.V/3.14159/N_a)**(1/3))/100 #m
+            radius = ((75 * soluteData.V / 3.14159 / N_a) ** (1. / 3)) / 100  # m
             diff = soluteData.getStokesDiffusivity(T, self.getSolventViscosity(T))
-            radii += radius #meters
+            radii += radius  # meters
             diffusivities += diff #m^2/s
         
-        k_diff = 4*3.14159*radii*diffusivities*N_a #m3/mol-s
+        k_diff = 4 * 3.14159 * radii * diffusivities * N_a  # m3/mol/s
         return k_diff
 
 
